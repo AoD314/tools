b48cc332081917f090e6ec31ace27a05c97459fc
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2013-02-26 00:56:48 +0400

    add ffmpeg script with working command patterns

---
+++ scripts/ffmpeg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+# ffmpeg -i dudek-000%04d.png -s 720x480 -r 30 -vcodec libxvid -b 1000k -aspect 3:2 out.mp4
+# ffmpeg -i dudek-000%04d.png -s 720x480 -r 30 -vcodec libx264 -b 1000k  out.mp4
+
+pass
