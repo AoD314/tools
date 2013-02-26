ce9a83a30bb621bb53a1103148ee40173ce70792
AoD314 <morozov.andrey.vmk@gmail.com>
2013-02-25 23:13:34 -0300

    improved download script

--- scripts/download_picture.py
+++ scripts/download_picture.py
@@ -8,17 +8,19 @@ def download_image(url, path):
 	urllib.request.urlretrieve(url, path)
 
 def main():
-	count_images = 500
+	start_frame  = 5200
+	count_images = 1800
 	for i in range(count_images):
-		url  = "http://blender-mirror.kino3d.org/peach/png/big_buck_bunny_00{0}.png".format(i + 100)
-		path = "big_buck_bunny_00{0}.png".format(i + 100)
+		# http://media.xiph.org/sintel/sintel-4k-png/00000001.png
+		# http://blender-mirror.kino3d.org/peach/png/big_buck_bunny_00000.png
+		url  = "http://blender-mirror.kino3d.org/peach/png/big_buck_bunny_{0:05}.png".format(i + start_frame)
+		path = "big_buck_bunny_{0:05}.png".format(i + start_frame)
 		download_image(url, path)
-		pr = (i + 1) * 100.0 / count_images
-		print("{0}%     :".format(pr), url, " --> ", path)
-		time.sleep(1) # for low level load on server
+
+		print("{:7.2%}   :  ".format(i / count_images), url, " --> ", path)
+		time.sleep(0.5) # for low level load on server
 	print('done.')
 	pass
 
 if __name__ == "__main__":
 	main()
-	
