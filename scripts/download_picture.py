818eedb7391de2fca6fab83e5fbc36b159fb89c5
AoD314 <morozov.andrey.vmk@gmail.com>
2013-03-20 00:40:35 -0300

    improved download script

--- scripts/download_picture.py
+++ scripts/download_picture.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+# http://media.xiph.org/sintel/sintel-4k-png/00000001.png
+# http://blender-mirror.kino3d.org/peach/png/big_buck_bunny_00000.png
 
 import urllib.request
-import os
-import time
+import os, time
 
 def download_image(url, path):
 	urllib.request.urlretrieve(url, path)
@@ -10,17 +11,21 @@ def download_image(url, path):
 def main():
 	start_frame  = 5200
 	count_images = 1800
+	folder_download = 'bbb-png'
+
+	if os.path.isdir(folder_download):
+		print("folder exist")
+		return
+	else:
+		os.mkdir(folder_download)
+
 	for i in range(count_images):
-		# http://media.xiph.org/sintel/sintel-4k-png/00000001.png
-		# http://blender-mirror.kino3d.org/peach/png/big_buck_bunny_00000.png
 		url  = "http://blender-mirror.kino3d.org/peach/png/big_buck_bunny_{0:05}.png".format(i + start_frame)
 		path = "big_buck_bunny_{0:05}.png".format(i + start_frame)
 		download_image(url, path)
-
-		print("{:7.2%}   :  ".format(i / count_images), url, " --> ", path)
-		time.sleep(0.5) # for low level load on server
+		print("{:7.2%}   :  ".format(i + 1 / count_images), url, " --> ", path)
+		time.sleep(1.0) # for low level load on server
 	print('done.')
-	pass
 
 if __name__ == "__main__":
 	main()
