63a540970accd19c3b227da1a43f073971f7199a
AoD314 <morozov.andrey.vmk@gmail.com>
2013-03-20 00:41:33 -0300

    fixed run ffmpeg

--- scripts/ffmpeg.py
+++ scripts/ffmpeg.py
@@ -1,8 +1,6 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 
-import os
-import time
-import argparse
+import os, subprocess, time, argparse
 
 def print_time(sec):
     h = int(sec / (60 * 60))
@@ -14,21 +12,22 @@ def print_time(sec):
     print('encode time : {0:02}:{1:02}:{2:02}.{3:02}'.format(h, m, s, ms))
 
 def create_cmd(fps, codec, bitrate, output):
-    input = "-i big_buck_bunny_052%02d.png "
+    input = "-i bbb-png-big/output_%05d.png "
 
     f = " -r {0}".format(fps)
 
-    c = " -vcodec "
-    if (codec == "XVID"): c += "libxvid -aspect 16:9"
+    c = " -codec:v "
+    if (codec == "XVID"): c += "libxvid "
     if (codec == "H264"): c += "libx264 "
 
-    b = " -b {0}".format(bitrate)
+    b = " -b:v {0}".format(bitrate)
 
-    return input + "{0} {1} {2} {3}".format(f, c, b, output)
+    return input + "{0} {1} {2} {3} -threads 12 ".format(f, c, b, output)
 
 def run_ffmpeg(cmd):
     start = time.time()
     print("\nRUN : ffmpeg " + cmd + '\n')
+    #subprocess.getoutput('ffmpeg ' + cmd)
     os.system('ffmpeg ' + cmd)
     finish = time.time()
     print_time(finish - start)
@@ -37,19 +36,17 @@ def main():
     parser = argparse.ArgumentParser(description='create video with FFmpeg.')
     parser.add_argument('-f', '--fps', type=float, default=30.0,
                         help='set fps for creating video')
-    parser.add_argument('-o', '--output', type=str, default="output.mp4",
+    parser.add_argument('-o', '--output', type=str, default="output.mkv",
                         help='set name of output video file')
-    parser.add_argument('-b', '--bitrate', type=str, default="8500k",
+    parser.add_argument('-b', '--bitrate', type=str, default="15000k",
                         help='set bitrate for output video file')
     parser.add_argument('-c', '--codec', choices=['XVID', 'H264', 'WebM'],
-                        default='XVID', help='set fps for creating video')
+                        default='H264', help='set fps for creating video')
     args = parser.parse_args()
 
     run_ffmpeg(" -version")
-    run_ffmpeg(create_cmd(args.fps, args.codec, args.bitrate, args.output) + \
-               " -pass 1 -an -y ")
-    run_ffmpeg(create_cmd(args.fps, args.codec, args.bitrate, \
-               '2' + args.output) + " -pass 2 -y ")
+    run_ffmpeg(create_cmd(args.fps, args.codec, args.bitrate, args.output) + " -pass 1 -an -y ")
+    run_ffmpeg(create_cmd(args.fps, args.codec, args.bitrate, args.output) + " -pass 2 -y ")
     pass
 
 if __name__ == "__main__":
