1e6d158afd3f8c70b66640f2d1443d92d7016f0b
AoD314 <morozov.andrey.vmk@gmail.com>
2013-05-02 15:51:59 -0300

    fix bug with drawing graph

--- scripts/run_experiments_imgs.py
+++ scripts/run_experiments_imgs.py
@@ -12,8 +12,9 @@ def write_to_file(text):
     f.write(text)
     f.close()
 
-experiments = ['4096x4096.png']
+experiments = ["../../imgs/1280x720.png", "../../imgs/1920x1080.png", "../../imgs/2880x1800.png", "../../imgs/3840x2160.png", "../../imgs/4096x4096.png", "../../imgs/7360x4912.png", "../../imgs/7680x4320.png", "../../imgs/8192x8192.png", "../../imgs/15360x8640.png"]
 
 for exp in experiments:
+    print(exp)
     output = run_experiment(exp)
     write_to_file(output)
--- scripts/run_test_img.py
+++ scripts/run_test_img.py
@@ -9,7 +9,29 @@ def run_experiments():
     text = f.read()
     f.close()    
     return text
-   
+
+################################## 
+
+class Table():
+    def __init__(self, frmt, enc, dec, cmpr, orig, params, psnr, w, h):
+        self.format = frmt
+        self.enc = float(enc)
+        self.dec = float(dec)
+        self.cmpr = float(cmpr)
+        self.orig = float(orig)
+        self.params = int(params)
+        self.psnr = float(psnr)
+        self.w = int(w)
+        self.h = int(h)
+    
+    def __repr__(self):
+        return repr((self.format, self.params, self.psnr, self.w, self.h, self.cmpr, self.orig, self.enc, self.dec))
+
+
+###################################
+
+
+
 def prase_all_output(out):
     list = out.split('}\n{')
     os = list[0].split('\n')
@@ -18,31 +40,42 @@ def prase_all_output(out):
 
     for i in list:
         l = i.split('\n')
-        d = {'format':'', 'enc':'', 'dec':'', 'cmpr_size':'', 'orig_size':'', 'params':'', 'psnr':'', 'w':'', 'h':''}
+        
+        frmt = '' 
+        enc = '' 
+        dec = '' 
+        cmpr = '' 
+        orig = ''
+        params = ''
+        psnr = ''
+        w = '' 
+        h = ''
 
         for j in l:            
             if j.find('format') >= 0:
-                d['format'] = j[j.find(":")+1:j.find(";")]
+                frmt = j[j.find(":")+1:j.find(";")]
             if j.find('params') >= 0:
-                d['params'] = j[j.find(":")+1:j.find(";")]
+                params = j[j.find(":")+1:j.find(";")]
             if j.find('enc') >= 0:
-                d['enc'] = j[j.find(":")+1:j.find(";")]
+                enc = j[j.find(":")+1:j.find(";")]
             if j.find('dec') >= 0:
-                d['dec'] = j[j.find(":")+1:j.find(";")]
-            if j.find('cmpr size') >= 0:
-                d['cmpr_size'] = j[j.find(":")+1:j.find(";")]
-            if j.find('orig_size') >= 0:
-                d['orig size'] = j[j.find(":")+1:j.find(";")]
+                dec = j[j.find(":")+1:j.find(";")]
+            if j.find('cmpr') >= 0:
+                cmpr = j[j.find(":")+1:j.find(";")]
+            if j.find('orig') >= 0:
+                orig = j[j.find(":")+1:j.find(";")]
             if j.find('PSNR') >= 0:
-                d['psnr'] = j[j.find(":")+1:j.find(";")]
+                psnr = j[j.find(":")+1:j.find(";")]
             if j.find('width') >= 0:
-                d['w'] = j[j.find(":")+1:j.find(";")]
+                w = j[j.find(":")+1:j.find(";")]
             if j.find('heigth') >= 0:
-                d['h'] = j[j.find(":")+1:j.find(";")]
+                h = j[j.find(":")+1:j.find(";")]
 
-        t.append(d)
+        t.append(Table(frmt, enc, dec, cmpr, orig, params, psnr, w, h))
 
     return t
+
+###################################
 
 def draw_plt(y, x, yt, xt, t, leg):
     """ amount of draw < 9 """
@@ -56,37 +89,58 @@ def draw_plt(y, x, yt, xt, t, leg):
     ind = np.arange(len(x))
     l = []
     for i in range(n):
-        l += [plt.bar(ind + i * width, y[i*len(x):(i+1)*len(x)], width, color=colors[i])[0]]
+        yy = y[i*len(x):(i+1)*len(x)]
+        l += [plt.bar(ind + i * width, yy, width, color=colors[i])[0]]
+
+        for j in range(len(yy)):
+            plt.text(ind[j] + i * width + width/2.0, yy[j] + (yy[j]/100), '%.1f' % float(yy[j]), fontsize=5, ha='center', va='bottom')
+
     lab = [str(i) for i in x]
     plt.xticks(ind + width * n / 2.0, lab)
-    plt.legend(l, leg)
-    plt.savefig(t.replace(' ', '_').replace('(','__').replace(')','__').lower() + '.svg')
+    plt.legend(l, leg, loc='best')
+    plt.savefig(t.replace(' ', '_').replace('(','__').replace(')','__').lower() + '.svg', dpi=314)
     #plt.show()
+
+###################################
+
 
 def grep_data_by(table, format, params, w, h):
     l = []
-    for i in table:
-        if format != '*' and i['format'] != format:
+    for t in table:
+        if format != '*' and t.format != format:
             continue
-        if params != '*' and i['params'] != params:
+        if params != '*' and t.params != params:
             continue
-        if w != '*' and i['w'] != w:
+        if w != '*' and t.w != int(w):
             continue
-        if h != '*' and i['h'] != h:
+        if h != '*' and t.h != int(h):
             continue
-        l += [i]
+        l += [t]
     return l
 
-def merge_arr(a1, a2):
+def merge_arr(a1, a2, a3 = []):
     l = []
-    for i in range(len(a1)):
-        l += [float(a1[i]), float(a2[i])]
+    l += a1
+    l += a2
+    if a3 != []:
+        l += a3
     return l
 
 def get_array_by_table(table, val):
     a = []
-    for i in table:
-        a += [i[val]]
+
+    tab = sorted(table, key=lambda t:t.params)
+
+    if val == 'params':
+        for t in tab:
+            a += [t.params]
+    elif val == 'psnr':
+        for t in tab:
+            a += [t.psnr]
+    elif val == 'cmpr':
+        for t in tab:
+            a += [t.cmpr]
+
     return a
 
 
@@ -94,37 +148,43 @@ def main():
     output = run_experiments()
     table = prase_all_output(output)
 
-    l = grep_data_by(table, '.webp', '*', '4096', '4096')
-    x = get_array_by_table(l, 'params')
+    res = [[1280, 720], [1920, 1080], [2880, 1800], [3840, 2160], [4096, 4096]]
+
+    for r in res:
+        l = grep_data_by(table, '.webp', '*', r[0], r[1])
+        x = get_array_by_table(l, 'params')
+
+        l = grep_data_by(table, '.webp', '*', r[0], r[1])
+        y_webp = get_array_by_table(l, 'psnr')
+
+        l = grep_data_by(table, '.jpeg', '*', r[0], r[1])
+        y_jpeg = get_array_by_table(l, 'psnr')
+        
+        l = grep_data_by(table, '.jp2', '*', r[0], r[1])
+        y_jp2 = get_array_by_table(l, 'psnr')
+
+        y = merge_arr(y_webp, y_jpeg, y_jp2)
+        
+        draw_plt(y, x, "PSNR", "Quality", 'WebP vs Jpeg vs Jpeg2000 (Quality-PSNR) [' + str(r[0]) + 'x' + str(r[1]) + ']', ['webp', 'jpeg', 'jpeg2000'])
 
 
+    for r in res:
+        l = grep_data_by(table, '.webp', '*', r[0], r[1])
+        x = get_array_by_table(l, 'params')
 
-    l = grep_data_by(table, '.webp', '*', '4096', '4096')
-    y_webp = get_array_by_table(l, 'psnr')
+        l = grep_data_by(table, '.webp', '*', r[0], r[1])
+        y_webp = get_array_by_table(l, 'cmpr')
 
-    l = grep_data_by(table, '.jpeg', '*', '4096', '4096')
-    y_jpeg = get_array_by_table(l, 'psnr')
+        l = grep_data_by(table, '.jpeg', '*', r[0], r[1])
+        y_jpeg = get_array_by_table(l, 'cmpr')
+        
+        l = grep_data_by(table, '.jp2', '*', r[0], r[1])
+        y_jp2 = get_array_by_table(l, 'cmpr')
 
-    y = merge_arr(y_webp, y_jpeg)
-    
-    draw_plt(y, x, "PSNR", "Quality", 'WebP vs Jpeg (Quality-PSNR)', ['webp', 'jpeg'])
-
-
-    l = grep_data_by(table, '.webp', '*', '4096', '4096')
-    y_webp = get_array_by_table(l, 'cmpr_size')
-
-    l = grep_data_by(table, '.jpeg', '*', '4096', '4096')
-    y_jpeg = get_array_by_table(l, 'cmpr_size')
-
-    y = merge_arr(y_webp, y_jpeg)
-    
-    draw_plt(y, x, "Compression Size(kb)", "Quality", 'WebP vs Jpeg (Quality-Compression Size)', ['webp', 'jpeg'])
-
-    #draw_plt([66, 68, 33, 35, 52, 55], [100, 90, 80], "YYY", 'XXX', 'test #02', ['only-x', 'only - y'])
-    #x = range(3)
-    #y = range(3*9)
-    #draw_plt(y, x, "3*9", '3', 'test #03', ['#ff0000','#00ff00','#0000ff','#ffff00','#ff00ff','#00ffff','#ff8000','#000000','#c0c0c0'])
-    pass
+        y = merge_arr(y_webp, y_jpeg, y_jp2)
+        
+        draw_plt(y, x, "Compression Size(kb)", "Quality", 'WebP vs Jpeg vs Jpeg2000 (Quality-PSNR) [' + str(r[0]) + 'x' + str(r[1]) + ']', ['webp', 'jpeg', 'jpeg2000'])
 
 if __name__ == "__main__":
     main()
+
